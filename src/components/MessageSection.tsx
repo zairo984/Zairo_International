2d94b3d87557735aa6559dfaea8f15d912c64c0e
manyagaur <manyagaur25@gmail.com>
2025-01-16 19:30:30 +0530

    added comp

--- src/components/MessageSection.tsx
+++ src/components/MessageSection.tsx
@@ -56,7 +56,7 @@ const MessageSection = () => {
                   }}
                 >
                   Discover More
-              </button>
+            </button>
         </div>
       </div>
     </div>
